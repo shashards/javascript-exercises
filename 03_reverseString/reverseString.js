247aa4b10aa6f1b65d2b2e35ebd9b80d49baf7c3
shashards <shane.shardlow@gmail.com>
2022-04-08 09:37:29 +0100

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArray = str.split("");
+    strArray.reverse();
+    const newStr = strArray.join("");
+    return newStr;
 };
+
+reverseString("Shane Shardlow")
 
 // Do not edit below this line
 module.exports = reverseString;
