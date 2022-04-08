cd409bd74bcf945429e355e2f3ec096dc9618d46
shashards <shane.shardlow@gmail.com>
2022-04-08 09:28:16 +0100

    complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newStr = "";
+    for (let i = 0; i < num; i++) {
+        newStr = newStr.concat(str);
+    }
+    return newStr
 };
+
+repeatString("Hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
