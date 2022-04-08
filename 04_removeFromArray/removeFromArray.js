a124b52d68e0667363fa4ad61cbc8e5b7edcc397
shashards <shane.shardlow@gmail.com>
2022-04-08 10:35:38 +0100

    Remove single arguments from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg) {
+    let checkNum = (i) => {
+        return arg != i
+
+    }  
+    const newArr = arr.filter(checkNum)
+    
+    let argArr = Array.from(arguments);
+    argArr.shift();
+
+    console.log(newArr);
+    return newArr
+
 
 };
+
+
+
+removeFromArray([1,2,3,4], 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
