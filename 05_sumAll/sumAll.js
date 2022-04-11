dcb1c4aa338821766d839f8b8bb5ed6915f174bc
shashards <shane.shardlow@gmail.com>
2022-04-11 21:37:53 +0100

    tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,32 +1,57 @@
 const sumAll = function(a, b) {
     //Change Arguments into Array
     let argArray = Array.from(arguments);
-    //Set High and Low Value
-    argArray.sort(function(a, b){return a-b});
-    let low = argArray[0];
-    let high = argArray[1];
-    //Create Variables
-    let finalSum = 0;
-    let range = [];
-    //push highest value into Range Array
-    range.push(high);
-    // loop through all the values from low up to high, and unshifting them into the array
-    for (let i = low; i < high; i++) {
-        range.unshift(i)
+    //loop through argArray and check for negative numbers
+    let arrCheck = false;
+    for (let i = 0; i < argArray.length; i++) {
+        if(negValueCheck(argArray[i])){
+            arrCheck = true
+        } else {
+            arrCheck = false
+            break
+        }
+    };
+    if(arrCheck == true){
+        //Set High and Low Value
+        argArray.sort(function(a, b){return a-b});
+        let low = argArray[0];
+        let high = argArray[1];
+        //Create Variables
+        let finalSum = 0;
+        let range = [];
+        //push highest value into Range Array
+        range.push(high);
+        // loop through all the values from low up to high, and unshifting them into the array
+        for (let i = low; i < high; i++) {
+            range.unshift(i)
+        }
+        // sort the array from low to high values
+        range.sort(function(low, high){return low-high})
+        // loop through range array adding values to finalSum
+        for (let j = 0; j < range.length; j++) {
+            finalSum += range[j];    
+        }
+        console.log(finalSum);
+        return finalSum;    
+    } else {
+            console.log("ERROR")
+            return "ERROR";
+            }
+        function negValueCheck(v){
+        if(v < 1){
+            return false;
+        } else { 
+            if(typeof v === "number"){
+                return true;
+            } else {
+                return false;
+            }
+        } 
     }
-    // sort the array from low to high values
-    range.sort(function(low, high){return low-high})
-    // loop through range array adding values to finalSum
-    for (let j = 0; j < range.length; j++) {
-        finalSum += range[j];    
-    }
-
-
-    
-    return finalSum;
 };
+sumAll(5, 10);
 
-sumAll(1, 4000)
+
 
 // Do not edit below this line
 module.exports = sumAll;
