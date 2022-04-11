0c91ed83ca4f77df3f2417cba06a2cc5d546ed11
shashards <shane.shardlow@gmail.com>
2022-04-11 15:00:39 +0100

    remove mutiple arguments from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,23 @@
-const removeFromArray = function(arr, arg) {
-    let checkNum = (i) => {
-        return arg != i
-
-    }  
-    const newArr = arr.filter(checkNum)
-    
-    let argArr = Array.from(arguments);
-    argArr.shift();
-
-    console.log(newArr);
-    return newArr
+const removeFromArray = function(arr, ...arg) {
+    for (let i = 0; i < arg.length; i++) {
+        let loopValue = arg[i];
+        if (arr.includes(loopValue)){
+            for (let j = 0; j < arr.length; j++) {
+                if(arr[j] === loopValue){
+                    arr.splice(j, 1);
+                };
+                
+            }
+        }
+        
+    } return console.log(arr);
 
 
 };
 
 
 
-removeFromArray([1,2,3,4], 4)
+removeFromArray([1,2,3,4], 1, 2, 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
