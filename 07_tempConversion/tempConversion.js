af6f9bb9701df447d97b6ed789cd98b2ed98bec6
shashards <shane.shardlow@gmail.com>
2022-04-25 10:37:23 +0100

    exercise passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * 5/9;
+  return Math.round(c * 10 ) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * 9/5 + 32;
+  return Math.round (f * 10 ) / 10;
 };
 
-// Do not edit below this line
+// Do not edit below this line 	// 	[°F] = [°C] × 9⁄5 + 32
 module.exports = {
   ftoc,
   ctof
